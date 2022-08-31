e72bfa16a2e8431e6a4f1337d2c5449657fed418
rizqisamawa <rizqisamawa11@gmail.com>
2022-09-01 02:28:42 +0700

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AddUsers from "./components/AddUsers";
+import UsersList from "./components/UsersList";
+import EditUsers from "./components/EditUsers";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<UsersList />} />
+      <Route path="add" element={<AddUsers />} />
+      <Route path="edit/:id" element={<EditUsers />} />
+    </Routes>
+  );
 };
 
 export default App;
