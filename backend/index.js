318473c6e8aeae097584ec07793151c7a58ce190
rizqisamawa <rizqisamawa11@gmail.com>
2022-09-02 19:54:16 +0700

    import fileupload

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express from "express";
+import FileUpload from "express-fileupload";
 import UserRouter from "./routes/UserRoute.js";
 
 const app = express();
 
 app.use(cors()); // adding middelware
 app.use(express.json()); // recieve request format json
+app.use(FileUpload()); // adding fileupload anything type file
 
 app.use(UserRouter);
 
