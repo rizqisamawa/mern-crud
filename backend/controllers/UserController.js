73a4a6483ac17321f3ec973691a8eea6ece28cd6
rizqisamawa <rizqisamawa11@gmail.com>
2022-09-03 13:47:35 +0700

    create users image controller

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 import users from "../models/UserModel.js";
+import path from "path";
 
 export const getUsers = async (req, res) => {
   try {
@@ -24,11 +25,43 @@ export const getUsersById = async (req, res) => {
 };
 
 export const createUsers = async (req, res) => {
-  try {
-    await users.create(req.body);
-    res.status(201).json({ msg: "User Created Successfully" });
-  } catch (err) {
-    console.log(err.message);
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No File Uploaded" });
+  } else {
+    const name = req.body.title;
+    const email = req.body.title;
+    const gender = req.body.title;
+    const file = req.files.file;
+    const fileSize = file.data.length;
+    const ext = path.extname(file.name);
+    const fileName = file.md5 + ext;
+    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+    const allowedType = [".png", ".jpg", ",jpeg"];
+
+    if (allowedType.includes(ext.toLowerCase())) {
+      return res.status(422).json({ msg: "Invalid Image" });
+    }
+    if (fileSize > 5000000) {
+      return res.status(422).json({ msg: "Image Must Be Less than 5MB" });
+    }
+
+    file.mv(`./public/images/${fileName}`, async (err) => {
+      if (err) {
+        return res.status(500).json({ msg: err.message });
+      }
+      try {
+        await users.create({
+          name: name,
+          email: email,
+          gender: gender,
+          image: fileName,
+          url: url,
+        });
+        res.status(201).json({ msg: "User Created Successfully" });
+      } catch (err) {
+        console.log(err.message);
+      }
+    });
   }
 };
 
