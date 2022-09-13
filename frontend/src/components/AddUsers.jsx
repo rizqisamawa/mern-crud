9397c57ad01f3aeff573a73fee9a3e4ce8d53482
rizqisamawa <rizqisamawa11@gmail.com>
2022-09-13 19:42:44 +0700

    add users

--- frontend/src/components/AddUsers.jsx
+++ frontend/src/components/AddUsers.jsx
@@ -6,15 +6,28 @@ const AddUsers = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [gender, setGender] = useState("Male");
+  const [file, setFile] = useState("");
+  const [preview, setPreview] = useState("");
   const navigate = useNavigate();
+
+  const loadImage = (e) => {
+    const image = e.target.files[0];
+    setFile(image);
+    setPreview(URL.createObjectURL(image));
+  };
 
   const createHandeler = async (e) => {
     e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("gender", gender);
+    formData.append("file", file);
     try {
-      await axios.post("http://localhost:5000/users", {
-        name,
-        email,
-        gender,
+      await axios.post("http://localhost:5000/users", formData, {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
       });
       navigate("/");
     } catch (err) {
@@ -22,7 +35,7 @@ const AddUsers = () => {
     }
   };
   return (
-    <div className="columns mt-5 is-centered">
+    <div className="columns is-centered mt-5">
       <div className="column is-half">
         <form onSubmit={createHandeler}>
           <div className="field">
@@ -31,9 +44,9 @@ const AddUsers = () => {
               <input
                 type="text"
                 className="input"
-                placeholder="Name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
+                placeholder="name"
               />
             </div>
           </div>
@@ -43,30 +56,54 @@ const AddUsers = () => {
               <input
                 type="email"
                 className="input"
-                placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                placeholder="email"
               />
             </div>
           </div>
           <div className="field">
             <label className="label">Gender</label>
             <div className="control">
-              <div className="select is-fullwidth">
-                <select
-                  value={gender}
-                  onChange={(e) => setGender(e.target.value)}
-                >
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                </select>
-              </div>
+              <input
+                type="text"
+                className="input"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+                placeholder="gender"
+              />
             </div>
           </div>
           <div className="field">
-            <button type="submit" className="button is-success">
-              Submit
-            </button>
+            <label className="label">Image</label>
+            <div className="control">
+              <div className="file">
+                <label className="file-label">
+                  <input
+                    type="file"
+                    className="file-input"
+                    onChange={loadImage}
+                  />
+                  <span className="file-cta">
+                    <span className="file-label">Choose a file...</span>
+                  </span>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          {preview && (
+            <figure className="image is-128x128">
+              <img src={preview} alt="preview image" />
+            </figure>
+          )}
+
+          <div className="field mt-5">
+            <div className="control">
+              <button type="submit" className="button is-success">
+                Save
+              </button>
+            </div>
           </div>
         </form>
       </div>
